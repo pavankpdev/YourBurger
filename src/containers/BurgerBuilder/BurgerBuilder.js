2821209aea2219b2740b4abd26ff2f5af4b3be5a
pavan kumar <pavankumarkpkp@gmail.com>
2019-10-01 22:33:07 +0530

    User Interaction Module

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,15 +1,62 @@
+/* eslint-disable no-lone-blocks */
 import React, { Component } from "react";
-import Aux from "../../hoc/Aux";
+import Aux from "../../hoc/Auxkp";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+const INGREDIENT_PRICES = {
+  Salad: 25,
+  Cheese: 30,
+  Meat: 35,
+  Bacon: 50
+};
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      Salad: 0,
+      Bacon: 0,
+      Cheese: 0,
+      Meat: 0
+    },
+    totalPrice: 10
+  };
+  addIngredientHandler = type => {
+    const oldCOunt = this.state.ingredients[type];
+    const updatedCount = oldCOunt + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
+
+  removeIngredientHandler = type => {
+    const oldCOunt = this.state.ingredients[type];
+    if (oldCOunt === 0) {
+      {
+        alert("Your Burger is Empty!");
+      }
+      return;
+    }
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = oldCOunt - 1;
+    const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
   render() {
     return (
       <Aux>
-        <Burger />
-        <div>Build Controls</div>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls
+          ingredientsAdded={this.addIngredientHandler}
+          ingredientsRemove={this.removeIngredientHandler}
+        />
       </Aux>
     );
   }
 }
-
 export default BurgerBuilder;
