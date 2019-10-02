3160a0cfdf8bf60245b773f756c4441c19cc223c
pavan kumar <pavankumarkpkp@gmail.com>
2019-10-02 16:21:09 +0530

    Module to generate order summary

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Aux from "../../../hoc/Auxkp";
+const orderSummary = props => {
+  const ingredientsSummary = Object.keys(props.ingredients).map(igKey => {
+    return (
+      <li key={igKey}>
+        <span style={{ textTransform: "capitalize" }}> {igKey}</span>: &#160;
+        {props.ingredients[igKey]}
+      </li>
+    );
+  });
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>{ingredientsSummary}</ul>
+      <p>Continue to checkout?</p>
+    </Aux>
+  );
+};
+
+export default orderSummary;
