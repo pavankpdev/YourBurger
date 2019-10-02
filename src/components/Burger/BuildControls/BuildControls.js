3104dbdfba310126a59de6121a72e40cd0f000d2
pavan kumar <pavankumarkpkp@gmail.com>
2019-10-02 16:09:25 +0530

    Burger Build Controls manager

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,14 +12,21 @@ const controls = [
 
 const buildControls = props => (
   <div className={classes.BuildControls}>
+    <p>
+      Current Price :<strong>₹{props.price.toFixed(2)}</strong>
+    </p>
     {controls.map(ctrl => (
       <BuildControl
         key={ctrl.label}
         label={ctrl.label}
         added={() => props.ingredientsAdded(ctrl.type)}
         remove={() => props.ingredientsRemove(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
       />
     ))}
+    <button className={classes.OrderButton} disabled={!props.purchasable}>
+      Order Now
+    </button>
   </div>
 );
 
