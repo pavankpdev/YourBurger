b6b6761084f343ed1c18ab3a11a9be2a11dd68d0
pavan kumar <pavankumarkpkp@gmail.com>
2019-10-03 21:26:24 +0530

    Checkout pop up

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,5 +1,20 @@
 import React from "react";
 import classes from "./Modal.module.css";
-const modal = props => <div className={classes.Modal}>{props.children}</div>;
+import Aux from "../../../hoc/Auxkp";
+import Backdrop from "../Backdrop/Backdrop";
+const modal = props => (
+  <Aux>
+    <Backdrop show={props.show} clicked={props.modalClosed} />
+    <div
+      className={classes.Modal}
+      style={{
+        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+        opacity: props.show ? "1" : "0"
+      }}
+    >
+      {props.children}
+    </div>
+  </Aux>
+);
 
 export default modal;
