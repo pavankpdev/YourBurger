acaf1cb10aea4fd4ea7e3f4bba77d9ce17890b54
pavan kumar <pavankumarkpkp@gmail.com>
2019-10-04 22:08:05 +0530

    Navigation manager

---
+++ src/components/Navigation/Navigationitems/NavigationItems.js
@@ -0,0 +1,16 @@
+import React from "react";
+import classes from "./NavigationItems.module.css";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+const navigationItems = () => (
+  <ul className={classes.NavigationItems}>
+    <NavigationItem link="/" active>
+      BurgerBuilder
+    </NavigationItem>
+    <NavigationItem link="/">
+      CheckOut
+    </NavigationItem>
+  </ul>
+);
+
+export default navigationItems;
