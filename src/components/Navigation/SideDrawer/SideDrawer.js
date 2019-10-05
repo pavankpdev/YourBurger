2e3f0acdffd21a9b89e77e38094150e4ef4ca7ae
pavan kumar <pavankumarkpkp@gmail.com>
2019-10-05 19:53:20 +0530

    Side Drawer for mobile device

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,14 +2,24 @@ import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../Navigationitems/NavigationItems";
 import classes from "./SideDrawer.module.css";
+import BackDrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Auxkp";
 const sideDrawer = props => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
+
   return (
-    <div className={classes.SideDrawer}>
-      <Logo />
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    <Aux>
+      <BackDrop clicked={props.closed} show={props.open} />
+      <div className={attachedClasses.join(" ")}>
+        <Logo height="25%" margin="25px" />
+        <nav>
+          <NavigationItems /> 
+        </nav>
+      </div>
+    </Aux>
   );
 };
 
