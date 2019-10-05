9fb2e483dbf6141b9a29ddfe4b64a400a4079387
pavan kumar <pavankumarkpkp@gmail.com>
2019-10-05 19:48:25 +0530

    Logo Module

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,7 +2,10 @@ import React from "react";
 import burgerLogo from "../../asset/images/Burger.svg";
 import classes from "./Logo.module.css";
 const logo = props => (
-  <div className={classes.Logo}>
+  <div
+    className={classes.Logo}
+    style={{ height: props.height, marginBottom: props.margin }}
+  >
     <img src={burgerLogo} alt="My Logo" />
   </div>
 );
