2268ef6580922057df53da34005576c43671206b
pavan kumar <pavankumarkpkp@gmail.com>
2019-10-08 19:10:12 +0530

    Main component to render application

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <BurgerBuilder />
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  state = {
+    show: true
+  };
+  componentDidMounty() {
+    setTimeout(() => {
+      this.setState({ show: false });
+    }, 5000);
+  }
+
+  render() {
+    return (
+      <div>
+        <Layout>{this.state.show ? <BurgerBuilder /> : null}</Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
